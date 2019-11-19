1965fad8047879612e04cc1258816c764dd254c8
Frankshi <frankshi@easyops.cn>
2019-11-19 17:46:31 +0800

    连接 mongoDB 成功

---
+++ zh-api/app/config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  connectionStr:"mongodb+srv://frankshi:frankshi@zhihu-unx7t.mongodb.net/test?retryWrites=true&w=majority"
+}
--- zh-api/app/index.js
+++ zh-api/app/index.js
@@ -4,6 +4,11 @@ const error = require('koa-json-error');
 const parameter = require('koa-parameter');
 const app = new Koa();
 const routing = require('./routes/index');
+const mongoose = require('mongoose');
+const { connectionStr } = require('./config');
+
+mongoose.connect(connectionStr,{ useNewUrlParser: true,useUnifiedTopology: true },()=> console.log('MongoDB 连接成功了'));
+mongoose.connection.on("error", (e)=> console.log(e));
 // 错误处理
 app.use(error({
   postFormat: (e,{stack,...rest})=>{
