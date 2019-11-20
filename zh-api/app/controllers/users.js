71e4cfd6eb580346400bbedb68d9d7d505ac3c68
Frankshi <frankshi@easyops.cn>
2019-11-20 14:51:52 +0800

    用户联库进行增删改查

--- zh-api/app/controllers/users.js
+++ zh-api/app/controllers/users.js
@@ -1,30 +1,40 @@
-const db = [{name:"lion"}];
+const User = require('../models/users');
 
 class UsersCtl{
-  find(ctx){
-    ctx.body = db;
+
+  async find(ctx){
+    ctx.body = await User.find();
   }
-  findById(ctx){
-    if(ctx.params.id * 1 >= db.length){
-      ctx.throw(412,'先决条件失败');
-    }
-    ctx.body = db[ctx.params.id];
+
+  async findById(ctx){
+    const user = await User.findById(ctx.params.id);
+    if(!user) { ctx.throw(404, '用户不存在')}
+    ctx.body = user;
   }
-  create(ctx){
+
+  async create(ctx){
     ctx.verifyParams({
       name: {type: 'string',required:true}
     });
-    db.push(ctx.request.body);
-    ctx.body = ctx.request.body;
+    const user = await new User(ctx.request.body).save();
+    ctx.body = user;
   }
-  update(ctx){
-    db[ctx.params.id * 1] = ctx.request.body;
-    ctx.body = ctx.request.body;
+
+  async update(ctx){
+    ctx.verifyParams({
+      name: {type: 'string',required:true}
+    });
+    const user = await User.findByIdAndUpdate(ctx.params.id,ctx.request.body);
+    if(!user) { ctx.throw(404, '用户不存在')}
+    ctx.body = user;
   }
-  deleteU(ctx){
-    db.splice(ctx.params.id * 1 , 1);
+
+  async deleteU(ctx){
+    const user = await User.findByIdAndRemove(ctx.params.id);
+    if(!user) { ctx.throw(404, '用户不存在')}
     ctx.status = 204;
   }
+
 }
 
 module.exports = new UsersCtl();
